bc1e7b456dabd2edd03530bffe4e49fd57f5c3be
riley.baptiste <riley.baptiste@OKCL007.ruralsource.com>
2018-11-06 15:15:24 -0600

    Initial commit

---
+++ CustomerBike/src/main/java/com/ruralsourcing/CustomerBike/CustomerBikeApplication.java
@@ -0,0 +1,12 @@
+package com.ruralsourcing.CustomerBike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerBikeApplication.class, args);
+	}
+}
